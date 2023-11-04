f31abad3c8960e7d8d2dc14b9b691a3f12fe3c7d
JonLur <51527242+JonLur@users.noreply.github.com>
2023-11-04 10:31:40 +0100

    Battery advarsel
    Forsøk på å få dette til å virke

--- battery.lua
+++ battery.lua
@@ -2,18 +2,14 @@
 --------------------------------------------------
 -- Handler directly called by the "low-level" watcher API.
 --------------------------------------------------
-pct_prev = nil
 function batt_watch_low()
   pct = hs.battery.percentage()
   minutes = hs.battery.timeRemaining()
-  if hs.battery.isCharging() then
-    hs.alert.show(string.format("PMachine is charging."))
-  else
+  if ( not( hs.battery.isCharging() )) then
     if pct < 8 then
       hs.alert.show(string.format("Plug-in the power NOW!  %02d minutes left!", minutes))
     end
   end
-  pct_prev = pct
 end
 --------------------------------------------------
 
