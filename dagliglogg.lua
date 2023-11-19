55465db92eb406053de0cd0a6367723ec158fae7
JonLur <51527242+JonLur@users.noreply.github.com>
2023-11-19 09:31:11 +0100

    Dagliglogg
    Mer generisk løsning på export. Lagt til spesifisering av kommandoer. Så mange endringer at det nok må testes litt mer.

--- dagliglogg.lua
+++ dagliglogg.lua
@@ -3,9 +3,11 @@
 -- File: dagliglogg.lua
 -- Author: Jon Lurås
 -- Date: 2023.11.12
--- Endret: 2023.11.17
--- Version: 2.0.7
+-- Endret: 2023.11.19
+-- Version: 2.0.8
 ------------------------
+
+kommandoer = "EXPORT STOPP"
 
 -- Database format:
 -- timestamp - YYYYMMDDHHMMSS
@@ -101,7 +103,7 @@ function get_last_lengde_text(db, lasttime)
 end
 
 
-function get_report(db, periode, periodesummering, rapportfilnavn)
+function get_report(db, periodestart, periodeslutt, periodesummering, rapportfilnavn)
     local outputfile = db_dagliglogg_directory .. rapportfilnavn
     local totalsum = 0
  
@@ -109,14 +111,14 @@ function get_report(db, periode, periodesummering, rapportfilnavn)
         os.remove(outputfile)
     end
 
-    local stmt = db:prepare("SELECT * FROM daglig WHERE timestamp > ?")
+    local stmt = db:prepare("SELECT * FROM daglig WHERE (timestamp > ?) and (timestamp < ?)")
     if not stmt then
         -- Handle the error (e.g., print an error message, log, or throw an exception)
         print("Error preparing SQL statement")
         return false, "Error preparing SQL statement"
     end
-    stmt:bind(1,periode)
-    
+    stmt:bind(1,periodestart)
+    stmt:bind(2,periodeslutt)
     -- Open the output file in append mode
     local file, file_error = io.open(outputfile, "a")
     if not file then
@@ -243,11 +245,9 @@ end
 function dato_start(periode)
     local startpunkt = nil
 
-    if (periode == "EXPORT") then
+    if (periode == 'DAG') then
         startpunkt = os.date("%Y%m%d000000")
-    elseif (periode == "EXPORT DAG") then
-        startpunkt = os.date("%Y%m%d000000")
-    elseif (periode == "EXPORT UKE") then
+    elseif (periode == 'UKE') then
 -- Function to calculate the first day of the week
         local now = os.time()
         local currentDayOfWeek = tonumber(os.date("%w", now))
@@ -266,7 +266,7 @@ function dato_start(periode)
             return nil
         end
         startpunkt = os.date("%Y%m%d000000", firstDayTimestamp)
-    elseif (periode == "EXPORT MND") then
+    elseif (periode == 'MND') then
         startpunkt = os.date("%Y%m01000000")
     else
         print("Unknown periode:", periode)
@@ -281,6 +281,10 @@ function dagliglogg_ny()
 -- select max(timestamp) from daglig 
 -- Kanskje putte teksten fra denne inn i bruker prompt
     local respons, beskrivelse, spaceindex, timeformat, lengde
+    local kontroll = false
+    local rapportsummering = false
+    local ukenr = nil
+    local dato = nil
 
     db_dagliglogg = hs.sqlite3.open(db_dagliglogg_directory .. db_dagliglogg_file)
 
@@ -321,63 +325,91 @@ function dagliglogg_ny()
 
     if (respons == "OK") then
         -- Remove all spaces in front and in end
+        local firstword = nil
+        local secondword = nil
         local beskrivelse = all_trim(beskrivelse)
         local uppertext = string.upper(beskrivelse)
+        if (string.find(uppertext,' ') ~= nil) then
+            firstword = string.sub(uppertext, 1, string.find(uppertext," ")-1)
+            secondword = string.sub(uppertext, string.find(uppertext," ")+1, string.len(uppertext))
+        else
+            firstword = uppertext
+        end
 
-        if (uppertext == 'STOPP') then
-            db_dagliglogg:exec("UPDATE " .. db_dagliglogg_table .. " SET lengde = " .. used_time .. " WHERE timestamp = '" .. last_time .. "' ")
-        elseif (uppertext == 'EXPORT') then
+        if (tonumber(secondword) ~= nil) then
+            if (string.len(secondword) == 6) then
+                ukenr = secondword
+            elseif (string.len(secondword) == 8) then
+                dato = secondword
+            end
+        end
+
+        if (string.find(kommandoer, firstword) ~= nil ) then
+            kontroll = true
+        end
+
+        if (kontroll) then
+            if (firstword == 'STOPP') then
+                db_dagliglogg:exec("UPDATE " .. db_dagliglogg_table .. " SET lengde = " .. used_time .. " WHERE timestamp = '" .. last_time .. "' ")
+            elseif (firstword == 'EXPORT') then
+                if ((secondword == nil) or (dato) or (secondword == 'DAG')) then
+                    if (dato) then
+                        fordato = os.time(time_from_string(dato))
+                    else
+                        fordato = os.time()
+                    end
+                    sluttdato = fordato + (60*60*24)
+                    start = os.date("%Y%m%d000000", fordato )
+                    if not start then
+                        print("Error:", "Feil med dato_start")
+                    end
 -- Eksporterer dagens dag til fil med dagensdato
-            start = dato_start('EXPORT')
-            if not start then
-                print("Error:", "Feil med dato_start")
-            end
-            local rapportsummering = true
-            rapportfilnavn = os.date("%Y%m%d.txt")
-            local success, error_message = get_report(db_dagliglogg, start, rapportsummering, rapportfilnavn)
-            if not success then
-                print("Error:", error_message)
-            end
-        elseif (uppertext == 'EXPORT DAG') then
--- Eksporterer dagens dag til fil med dagensdato
-            start = dato_start('EXPORT DAG')
-            if not start then
-                print("Error:", "Feil med dato_start")
-            end
-            local rapportsummering = true
-            rapportfilnavn = os.date("%Y%m%d.txt")
-            local success, error_message = get_report(db_dagliglogg, start, rapportsummering, rapportfilnavn)
-            if not success then
-                print("Error:", error_message)
-            end
-        elseif (uppertext == 'EXPORT UKE') then
+                    rapportsummering = true
+                    rapportfilnavn = os.date("%Y%m%d.txt", fordato)
+                    local success, error_message = get_report(db_dagliglogg, start, stopp, rapportsummering, rapportfilnavn)
+                    if not success then
+                        print("Error:", error_message)
+                    end
+                elseif ((secondword == 'UKE') or (ukenr)) then
 -- Eksporterer denne uka til fil med denne ukenr
-            start = dato_start('EXPORT UKE')
-            if not start then
-                print("Error:", "Feil med dato_start")
-            end
-            local rapportsummering = false
-            rapportfilnavn = os.date("%Yuke%W.txt")
-            local success, error_message = get_report(db_dagliglogg, start, rapportsummering, rapportfilnavn)
-            if not success then
-                print("Error:", error_message)
-            end
-        elseif (uppertext == 'EXPORT MND') then
+                    if (ukenr) then
+                        startdato = getFirstDateOfWeek(string.sub(ukenr,1,4), string.sub(ukenr,5,6))
+                    else
+                        startdato = dato_start(secondword)
+                    end
+                    if not startdato then
+                        print("Error:", "Feil med dato_start")
+                    end
+                    stoppdato = startdato + (60*60*24*7)
+                    if not stoppdato then
+                        print("Error:", "Feil med dato_start")
+                    end
+                    start = os.date("%Y%m%d000000", startdato )
+                    stopp = os.date("%Y%m%d000000", stoppdato )
+                    local rapportsummering = false
+                    rapportfilnavn = os.date("%Yuke%W.txt")
+                    local success, error_message = get_report(db_dagliglogg, start, stopp, rapportsummering, rapportfilnavn)
+                    if not success then
+                        print("Error:", error_message)
+                    end
+                elseif (secondword == 'MND') then
 -- Eksporterer denne uka til fil med denne måneder
-            start = dato_start('EXPORT MND')
-            if not start then
-                print("Error:", "Feil med dato_start")
-            end
-            local rapportsummering = false
-            rapportfilnavn = os.date("%Ymnd%m.txt")
-            local success, error_message = get_report(db_dagliglogg, start, rapportsummering, rapportfilnavn)
-            if not success then
-                print("Error:", error_message)
+                    start = dato_start('MND')
+                    if not start then
+                        print("Error:", "Feil med dato_start")
+                    end
+                    local rapportsummering = false
+                    rapportfilnavn = os.date("%Ymnd%m.txt")
+                    local success, error_message = get_report(db_dagliglogg, start, rapportsummering, rapportfilnavn)
+                    if not success then
+                        print("Error:", error_message)
+                    end
+                end
             end
         else
-           oppgave(db_dagliglogg, current_time, beskrivelse) 
-        end;
-    end;
+            oppgave(db_dagliglogg, current_time, beskrivelse) 
+        end
+    end
 
     db_dagliglogg:close()
 
